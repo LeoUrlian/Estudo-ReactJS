cafd9f77b404ecc833b5f9f763b26d0d10d3c5ed
Leonardo Sudati <leourlian@hotmail.com>
2022-05-31 14:54:53 -0300

    aula 3

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,15 +2,23 @@ import React from "react";
 
 import Botao from "../Botao"
 
+import style from "./Formulario.module.scss"
+
+/**
+ * <div className={style["novaTarefa__-container"]}> 
+ * se o estilo precisar de um hifen é necessário 
+ * chamalá do jeito acimal
+ */
+
 class Formulario extends React.Component{
   render() {
     return(
-      <form action="">
-        <div>
+      <form action="" className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required />
         </div>
-        <div>
+        <div className={style.inputContainer}>
         <label htmlFor="tempo">Tempo</label>
           <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required/>
         </div>
