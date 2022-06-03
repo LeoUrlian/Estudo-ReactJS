36a7e8da3311d2d8bac0b848fe88d1dcf2569832
Leonardo Sudati <leourlian@hotmail.com>
2022-06-03 17:27:19 -0300

    aula final

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -8,10 +8,11 @@ import Relogio from "./Relogio";
 
 interface Props{
   selecionado: ITarefa | undefined;
+  finalizarTarefa: () => void
 }
 
-export default function Cronometro({selecionado}: Props){
-  const [tempo, setTempo] = useState<Number>();
+export default function Cronometro({selecionado, finalizarTarefa}: Props){
+  const [tempo, setTempo] = useState<number>();
 
   useEffect(() => {
     if(selecionado?.tempo){
@@ -19,16 +20,23 @@ export default function Cronometro({selecionado}: Props){
     }
   }, [selecionado])
 
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if(contador > 0){
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+      finalizarTarefa();
+    }, 1000)
+  }
+
   return(
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-      <>
-        Tempo: {tempo}
-      </>
       <div className={style.relogioWrapper}>
-        <Relogio/>
+        <Relogio tempo={tempo}/>
       </div>
-      <Botao>
+      <Botao onClick={() => regressiva(tempo)}>
         Começar!
       </Botao>
     </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
+
 import Lista from '../components/Lista';
 import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
@@ -18,6 +19,24 @@ function App() {
     })))
   }
 
+  function finalizarTarefa() {
+    if(selecionado){
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => 
+        tarefasAnteriores.map(tarefa => {
+          if(tarefa.id === selecionado.id){
+            return {
+              ...tarefa,
+              selecionado: false,
+              completado: true
+            }
+          }
+          return tarefa;
+        })
+      )
+    }
+  }
+
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
@@ -25,7 +44,10 @@ function App() {
         tarefas={tarefas} 
         selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro selecionado={selecionado} />
+      <Cronometro 
+        selecionado={selecionado} 
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
